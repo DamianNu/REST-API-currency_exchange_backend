19b15652778c082d9dd1552ee0434e1ba82e7f1f
DamianNu <damian21v@gmail.com>
2023-12-23 23:03:49 +0100

    rejestracja

--- src/main/java/com/kodilla/currencyexchange/controller/UserController.java
+++ src/main/java/com/kodilla/currencyexchange/controller/UserController.java
@@ -7,6 +7,8 @@ import com.kodilla.currencyexchange.repository.UserRepository;
 import com.kodilla.currencyexchange.service.DbServiceUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
@@ -15,7 +17,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
 
@@ -27,9 +29,15 @@ public class UserController {
     private final DbServiceUser serviceUser;
 
     @RequestMapping(method = RequestMethod.POST)
-    public void createUser(@Valid @RequestBody User user) {
-        user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
-        userRepository.save(user);
+    public ResponseEntity<Void> createUser(@Valid @RequestBody User user) {
+        User user1 = userRepository.findByName(user.getName());
+        if(user1 != null) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+        } else {
+            user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
+            userRepository.save(user);
+            return ResponseEntity.ok().build();
+        }
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{userName}")
@@ -47,43 +55,4 @@ public class UserController {
     public ResponseEntity<UserValid> getUserByEmail(@PathVariable String userEmail) throws UserNotFoundException {
         return ResponseEntity.ok(userMapper.mapToUserValid(serviceUser.getUserByEmail(userEmail)));
     }
-
-//    @RequestMapping(method = RequestMethod.PUT)
-//    public ResponseEntity<UserValid> updateUserEmail(@Valid @RequestBody UserValid userValid) {
-//        User user = userRepository.findByName(userValid.getName());
-//        User saveUser = new User(user.getId(),user.getName(),userValid.getEmail(),passwordEncoder.encode(user.getPassword());
-//        serviceUser.saveUser(saveUser);
-//        return ResponseEntity.ok(userMapper.mapToUserValid(saveUser));
-//    }
-
-//    @RequestMapping(method = RequestMethod.PUT)
-//    public ResponseEntity<User> updateUser(@RequestBody User user) {
-//
-//        // Sprawdź, czy użytkownik jest zalogowany
-//        if (!SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
-//            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-//        }
-//
-//        // Sprawdź, czy użytkownik jest właścicielem rekordu
-////        User currentUser = getCurrentUser();
-////        if (id != currentUser.getId()) {
-////            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
-////        }
-//
-//        // Uaktualnij hasło
-//        if (user.getPassword() != null) {
-//            user.setPassword(passwordEncoder.encode(user.getPassword()));
-//        }
-//
-//        // Uaktualnij adres e-mail
-//        if (user.getEmail() != null) {
-//            user.setEmail(userRepository.findByEmail(user.getEmail()).orElse(null));
-//        }
-//
-//        // Zapisz użytkownika w bazie danych
-//        userRepository.save(user);
-//
-//        // Zwróc odpowiedź HTTP
-//        return ResponseEntity.status(HttpStatus.OK).body(user);
-//    }
 }
